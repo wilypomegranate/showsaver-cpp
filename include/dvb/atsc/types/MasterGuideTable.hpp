36c0cc8430230f671b90f7d0659baafc48295ce5
wilypomegranate <wilypomegranate@users.noreply.github.com>
2018-01-14 18:11:50 -0500

    Declaration of MasterGuideTable.

---
+++ include/dvb/atsc/types/MasterGuideTable.hpp
@@ -0,0 +1,49 @@
+#include "dvb/utils/ParserUtils.hpp"
+#include <bitset>
+#include <cstdint>
+#include <vector>
+
+namespace showsaver {
+namespace dvb {
+namespace atsc {
+class PsipTable {
+public:
+  PsipTable();
+
+private:
+  std::uint16_t table_type_;
+  std::bitset<3> reserved_;
+  std::bitset<13> table_type_pid_;
+  std::bitset<3> reserved2_;
+  std::bitset<5> table_type_version_number_;
+  std::uint32_t num_bytes_;
+  std::bitset<4> reserved3_;
+  std::bitset<12> table_type_descriptors_length_;
+};
+class MasterGuideTable {
+public:
+  MasterGuideTable();
+  std::uint16_t init(const std::vector<unsigned char> &, const std::uint16_t);
+
+private:
+  std::uint8_t table_id_;
+  std::bitset<1> section_syntax_indicator_;
+  std::bitset<1> private_indicator_;
+  std::bitset<2> reserved_;
+  std::bitset<12> section_length_;
+  std::uint16_t table_id_extension_;
+  std::bitset<2> reserved2_;
+  std::bitset<5> version_number_;
+  std::bitset<1> current_next_indicator_;
+  std::uint8_t section_number_;
+  std::uint8_t last_section_number_;
+  std::uint8_t protocol_version_;
+  std::uint16_t tables_defined_;
+  std::vector<PsipTable> tables_;
+  std::bitset<4> reserved3_;
+  std::bitset<12> descriptors_length_;
+  std::uint32_t crc_;
+};
+}
+}
+}
