f11651dbc718f50d063e2f1533ea8d891cb75da8
wilypomegranate <wilypomegranate@users.noreply.github.com>
2018-01-20 20:21:36 -0500

    Added clang format for EIT.

--- src/dvb/atsc/types/EventInformationTable.cpp
+++ src/dvb/atsc/types/EventInformationTable.cpp
@@ -8,22 +8,23 @@ EventInformationSection::EventInformationSection() {}
 
 EventInformationTable::EventInformationTable() {}
 
-  std::uint16_t EventInformationSection::init(const std::vector<unsigned char>&buffer) {
+std::uint16_t
+EventInformationSection::init(const std::vector<unsigned char>& buffer) {
   std::uint16_t parsed_bytes = 0;
 
   reserved_ = buffer[parsed_bytes] >> 6;
-  utils::parse_uint16_t(buffer.begin()+parsed_bytes, event_id_);
+  utils::parse_uint16_t(buffer.begin() + parsed_bytes, event_id_);
   event_id_ &= 0x3FFF;
 
   parsed_bytes += 2;
 
-  utils::parse_uint32_t(buffer.begin()+parsed_bytes, start_time_);
+  utils::parse_uint32_t(buffer.begin() + parsed_bytes, start_time_);
   parsed_bytes += 4;
 
   reserved2_ = buffer[parsed_bytes] >> 6;
   etm_location_ = buffer[parsed_bytes] >> 6;
 
-  utils::parse_uint32_t(buffer.begin()+parsed_bytes, length_in_seconds_);
+  utils::parse_uint32_t(buffer.begin() + parsed_bytes, length_in_seconds_);
   length_in_seconds_ &= 0xFFFFF00;
   length_in_seconds_ = length_in_seconds_ << 8;
   parsed_bytes += 3;
@@ -31,13 +32,14 @@ EventInformationTable::EventInformationTable() {}
   title_length_ = buffer[parsed_bytes];
   parsed_bytes += 1;
 
-  // std::cout.write(reinterpret_cast<const char *>(buffer.data()+parsed_bytes), title_length_);
-  // std::cout << std::endl;
-  title_text_ = std::string(buffer.begin()+parsed_bytes, buffer.begin()+parsed_bytes+title_length_);
+  // std::cout.write(reinterpret_cast<const char *>(buffer.data()+parsed_bytes),
+  // title_length_); std::cout << std::endl;
+  title_text_ = std::string(buffer.begin() + parsed_bytes,
+                            buffer.begin() + parsed_bytes + title_length_);
   parsed_bytes += title_length_;
 
   reserved3_ = buffer[parsed_bytes] << 4;
-  utils::parse_uint16_t(buffer.begin()+parsed_bytes, descriptors_length_);
+  utils::parse_uint16_t(buffer.begin() + parsed_bytes, descriptors_length_);
   descriptors_length_ &= 0xFFF;
   parsed_bytes += 2;
 
@@ -46,13 +48,14 @@ EventInformationTable::EventInformationTable() {}
   return parsed_bytes;
 }
 
-  std::uint16_t EventInformationTable::init(const std::vector<unsigned char> &buffer,
-                   std::uint16_t size) {
+std::uint16_t
+EventInformationTable::init(const std::vector<unsigned char>& buffer,
+                            std::uint16_t size) {
   std::uint16_t parsed_bytes = 0;
 
   parsed_bytes += dvb::utils::parse_header(buffer, table_id_, section_length_);
 
-  utils::parse_uint16_t(buffer.begin()+parsed_bytes, source_id_);
+  utils::parse_uint16_t(buffer.begin() + parsed_bytes, source_id_);
   parsed_bytes += 2;
 
   reserved2_ = buffer[parsed_bytes] >> 6;
@@ -78,20 +81,22 @@ EventInformationTable::EventInformationTable() {}
     parsed_bytes += add_event_section(table_buffer);
   }
 
-  utils::parse_uint32_t(buffer.begin()+parsed_bytes, crc_);
+  utils::parse_uint32_t(buffer.begin() + parsed_bytes, crc_);
   parsed_bytes += 4;
 
-  // std::cout << dvb::utils::validate_crc_32(buffer.data(), parsed_bytes, crc_) << std::endl;
+  // std::cout << dvb::utils::validate_crc_32(buffer.data(), parsed_bytes, crc_)
+  // << std::endl;
 
   return parsed_bytes;
 }
 
-std::uint16_t EventInformationTable::add_event_section(const std::vector<unsigned char>& buffer) {
+std::uint16_t EventInformationTable::add_event_section(
+    const std::vector<unsigned char>& buffer) {
   EventInformationSection section;
   std::uint16_t parsed_bytes = section.init(buffer);
   sections_.push_back(section);
   return parsed_bytes;
-  }
 }
-}
-}
+} // namespace atsc
+} // namespace dvb
+} // namespace showsaver
